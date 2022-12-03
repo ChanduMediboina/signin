502aa80af54669942ff6f99ab98c0b6b0190c3e9
ChanduMediboina <chandumediboina95@gmail.com>
2022-12-03 07:48:32 +0000

    another way of adding cors

--- app.js
+++ app.js
@@ -11,8 +11,14 @@ const app = express();
 
 app.use(express.json());
 
-//cors
-app.use(cors({ Credentials: true }));
+app.use(
+  cors({
+    origin: "*",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+  })
+);
 
 const dbPath = path.join(__dirname, "userData.db");
 
